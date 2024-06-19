352d5d47c35f379279f6bb6426cb052a754b1e8b
aivaneezy <aivannn1999@gmail.com>
2024-06-19 13:07:07 +0200

    Fix suspense register

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,34 +1,36 @@
 import { connectDB } from "@/app/lib/mongoose";
 import User from "@/app/models/User";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import { NextResponse } from "next/server";
-export const POST = async(request) =>{
-    if(request.method == "POST"){
-        const {username,  password, email} = await request.json();
-        await connectDB();
 
-        if(!username || !password){
-            return new NextResponse("Please provide a username and password", {status:400});
-        }
+export const POST = async (request) => {
+  if (request.method === "POST") {
+    try {
+      const { username, password, email, role = "user" } = await request.json();
+      await connectDB();
 
-        try{
-            // Check if the user already exists;
-            const existUser = await User.findOne({username});
-            if(existUser){
-                return new NextResponse("User already exist", {status:400});
-            }
+      if (!username || !password) {
+        return new NextResponse("Please provide a username and password", { status: 400 });
+      }
 
-            //hashing password
-            const hashedPassword = await bcrypt.hash(password, 10);
+      // Check if the user already exists
+      const existUser = await User.findOne({ username });
+      if (existUser) {
+        return new NextResponse("User already exists", { status: 400 });
+      }
 
-            // Creating a new User;
-            await User.create({username: username, email:email, password: hashedPassword, });
+      // Hashing password
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-            return new NextResponse("User is created succesfully", {status:200});
-        }catch(err){
-            return new NextResponse(err.message, {status:500});
-        }
-    }else{
-        return new NextResponse("Method is not allowed", {status:405});
+      // Creating a new User
+      await User.create({ username, email, password: hashedPassword, role });
+
+      return new NextResponse("User is created successfully", { status: 200 });
+    } catch (err) {
+      console.error("Error creating user:", err);
+      return new NextResponse(err.message, { status: 500 });
     }
-}
+  } else {
+    return new NextResponse("Method not allowed", { status: 405 });
+  }
+};
--- app/models/User.js
+++ app/models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String },
  userId: {type:String},
-  role: { type: String, required: true }
+ role: { type: String, default: "user" }
 });
 
 const User = mongoose.models.User || mongoose.model("User", UserSchema);
